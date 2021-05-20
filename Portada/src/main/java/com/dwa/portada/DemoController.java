5e239cc7a489d53e8780479c07b28842fe8c4051
brion <brion@DESKTOP-QJG6945>
2021-05-20 16:39:53 -0500

    Subido

---
+++ Portada/src/main/java/com/dwa/portada/DemoController.java
@@ -0,0 +1,13 @@
+package com.dwa.portada;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller 
+public class DemoController {
+
+	@GetMapping("/Portada")
+	public String Portada() {
+		return "/Portada";
+	}
+}
