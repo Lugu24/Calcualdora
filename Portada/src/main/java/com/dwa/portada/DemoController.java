a937927c3129d08a899d801d4156fa2f6c483a34
brion <brion@DESKTOP-QJG6945>
2021-05-20 17:24:22 -0500

    Subido

--- Portada/src/main/java/com/dwa/portada/DemoController.java
+++ Portada/src/main/java/com/dwa/portada/DemoController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller 
 public class DemoController {
 
-	@GetMapping("/Portada")
+	@GetMapping("/Demo/Portada")
 	public String Portada() {
-		return "/Portada";
+		return "Portada";
 	}
 }
